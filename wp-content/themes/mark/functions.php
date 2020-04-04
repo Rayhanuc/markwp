8256273bef94e94eaa48ec44a6f29f095beec100
[Rayanuc] <[rayhanctg4@gmail.com]>
2020-04-04 11:25:56 +0600

    ২.৩ - অ্যাসেটস ম্যানেজমেন্ট - থিমে সিএসএস ফাইল গুলো এনকিউ করা
    ২.৩ - অ্যাসেটস ম্যানেজমেন্ট - থিমে সিএসএস ফাইল গুলো এনকিউ করা

--- wp-content/themes/mark/functions.php
+++ wp-content/themes/mark/functions.php
@@ -1,6 +1,7 @@
 <?php
 
-
+define("VERSION", time());
+define("ASSETS_DIR", get_template_directory_uri()."/assets/");
 function mark_theme_setup() {
     load_theme_textdomain('mark', get_template_directory_uri()."/languages");
     add_theme_support( 'post-thumbnails' );
@@ -18,4 +19,31 @@ function mark_theme_setup() {
 }
 add_action('after_setup_theme', 'mark_theme_setup');
 
+function mark_assets(){
+    $css_files = array(
+        'google-fonts-css' => ASSETS_DIR.'//fonts.googleapis.com/css?family=Cabin|Open+Sans:300,400,600,700',
+        'fontawesome-css' => ASSETS_DIR.'vendor/bootstrap/css/bootstrap.min.css',
+        // 'bootstrap-css' => ASSETS_DIR.'vendor/font-awesome/css/font-awesome.min.css',
+        'bicon-css' => ASSETS_DIR.'vendor/bicon/css/bicon.css',
+        'owlcarousel-css' => ASSETS_DIR.'vendor/owl/assets/owl.carousel.min.css',
+        'owlcarousel-theme-css' => ASSETS_DIR.'vendor/owl/assets/owl.theme.default.min.css',
+        'magnific-popup-css' => ASSETS_DIR.'vendor/magnific-popup/magnific-popup.css',
+        'animate-css' => ASSETS_DIR.'vendor/animate.css',
+        'mark-main-css' => ASSETS_DIR.'css/main.css',
+    );
 
+    foreach($css_files as $handle => $css_file){
+        wp_enqueue_style($handle, $css_file,null, VERSION);
+    }
+
+    wp_enqueue_style('mark-css', get_stylesheet_uri(), null, VERSION);
+}
+add_action('wp_enqueue_scripts','mark_assets');
+
+
+/*
+    <!--animate css-->
+    <link rel="stylesheet" href="assets/vendor/animate.css">
+
+    <!--custom css-->
+    <link href="assets/css/main.css" rel="stylesheet"> */
